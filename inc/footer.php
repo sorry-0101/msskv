7b34f523e7f257778b00062be35ad8c27474d00e
Neeraj Kumar <98740620+sorry-0101@users.noreply.github.com>
2022-05-29 15:03:14 +0530

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,57 @@
+<footer class="footer-area pt-30 pb-30">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<div class="single-contact-address d-flex flex-row">
+					<div class="icon">
+						<span class="lnr lnr-home p-2 text-white "></span>
+					</div>
+					<div class="contact-details">
+						<h5 class="text-white">Khwajpur Meerpur</h5>
+						<p class="text-white">
+							Sikarpur Road Bulandshahr (203001)
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="single-contact-address d-flex flex-row">
+					<div class="icon">
+						<span class="lnr lnr-phone-handset p-2 text-white"></span>
+					</div>
+					<div class="contact-details">
+						<h5 class="text-white">9456842609</h5>
+						<p class="text-white">Mon to Sat 8:00 am to 2:00 pm</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="single-contact-address d-flex flex-row">
+					<div class="icon">
+						<span class="lnr lnr-envelope p-2 text-white"></span>
+					</div>
+					<div class="contact-details">
+						<h5 class="text-white">msskvumv.khwajpurmeerpur.1237@gmail.com</h5>
+						<p class="text-white">Send us your query anytime!</p>
+					</div>
+				</div>
+			</div>
+
+
+
+
+
+
+		</div>
+
+		<div class="footer-bottom row align-items-center justify-content-between">
+			<p class="footer-text m-0 col-lg-6 col-md-12">
+				Copyright &copy;
+				<script>
+					document.write(new Date().getFullYear());
+				</script> All rights reserved M S S K V U M V
+			</p>
+
+		</div>
+	</div>
+</footer>
---
+++ inc/header.php
@@ -0,0 +1,76 @@
+ <style>
+ 	.logo_class {
+ 		color: #fff;
+ 		font-size: 22px;
+ 		font-family: inherit;
+ 		font-weight: 500;
+		 padding-left: 20px;
+ 	}
+ </style>
+
+
+ <!-- #header -->
+ <header id="header" id="home">
+ 	<div class="header-top">
+ 		<div class="container">
+ 			<div class="row">
+ 				
+ 				<div class="col-lg-12 col-sm-12 col-12 header-top-right no-padding">
+ 				
+ 					<a href="tel:+919456842609"><span class="lnr lnr-phone-handset"></span> <span class="text">9456842609</span></a>
+ 					<a href="mailto:msskvumv.khwajpurmeerpur.1237@gmail.com"><span class="lnr lnr-envelope"></span> <span class="text">msskvumv.khwajpurmeerpur.1237@gmail.com</span></a>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</div>
+ 	<div class="container-fluid main-menu ">
+ 		<div class="row align-items-center justify-content-between d-flex">
+ 			<div id="logo">
+ 				<a href="index.html">
+
+ 					<span class="logo_class ">M S S K V U M V</span>
+ 				</a>
+ 			</div>
+ 			<nav id="nav-menu-container" style="padding-right: 10px;">
+ 				<ul class="nav-menu">
+ 					<li><a href="index.html">Home</a></li>
+ 					<li class="menu-has-children"><a href="#">About</a>
+ 						<ul>
+ 							<li><a href="#">Principal’s Message</a></li>
+ 							<li><a href="#">Manager's Message</a></li>
+ 						</ul>
+ 					</li>
+ 					<li><a href="#">Teachers</a></li>
+
+
+ 					<!-- <li><a href="about.html">About</a></li> -->
+ 					<!-- <li><a href="courses.html">Courses</a></li> -->
+ 					<!-- <li><a href="events.html">Events</a></li> -->
+ 					<!-- <li><a href="gallery.html">Gallery</a></li> -->
+ 					<li class="menu-has-children"><a href="#">Gallery</a>
+ 						<ul>
+ 							<li><a href="gallery.html">Library</a></li>
+ 							<li><a href="sports.html">Sport Ground</a></li>
+ 							<li><a href="events.html">Events</a></li>
+ 						</ul>
+ 					</li>
+ 					<!-- <li class="menu-has-children"><a href="">Pages</a>
+  						<ul>
+  							<li><a href="course-details.html">Course Details</a></li>
+  							<li><a href="event-details.html">Event Details</a></li>
+  							<li><a href="elements.html">Elements</a></li>
+  							<li class="menu-has-children"><a href="">Level 2 </a>
+  								<ul>
+  									<li><a href="#">Item One</a></li>
+  									<li><a href="#">Item Two</a></li>
+  								</ul>
+  							</li>
+  						</ul>
+  					</li> -->
+ 					<li><a href="contact.html">Contact</a></li>
+ 				</ul>
+ 			</nav><!-- #nav-menu-container -->
+ 		</div>
+ 	</div>
+ </header>
+ <!-- #header -->
